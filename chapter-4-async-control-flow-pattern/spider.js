37bef3be1053d72f600d9a9af5fadd6cff45bc87
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-12-11 18:19:12 +0300

    fix callback hell with call back using callback discipline

--- chapter-4-async-control-flow-pattern/spider.js
+++ chapter-4-async-control-flow-pattern/spider.js
@@ -7,25 +7,39 @@ import { urlToFilename } from "./utils.js";
 export function spider(url, cb) {
   const filename = urlToFilename(url);
   fs.access(filename, (err) => {
-    if (err && err.code === "ENOENT") {
-      console.log(`Downloading ${url} into ${filename}`);
-      superagent.get(url).end((err, res) => {
-        if (err) {
-          cb(err);
-        } else {
-          mkdirp(path.dirname(filename)).then((made) => {
-            fs.writeFile(filename, res.text, (err) => {
-              if (err) {
-                cb(err);
-              } else {
-                cb(null, filename, true);
-              }
-            });
-          });
-        }
-      });
-    } else {
-      cb(null, filename, false);
+    if (!err || err.code !== "ENOENT") {
+      return cb(null, filename, false);
     }
+
+    download(url, filename, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, filename, true);
+    });
+  });
+}
+
+function saveFile(filename, contents, cb) {
+  mkdirp(path.dirname(filename))
+    .then((_) => {
+      fs.writeFile(filename, contents, cb);
+    })
+    .catch((err) => file);
+}
+
+function download(url, filename, cb) {
+  console.log(`Downloading ${url} into ${filename}`);
+  superagent.get(url).end((err, res) => {
+    if (err) {
+      return cb(err);
+    }
+    saveFile(filename, res.text, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      cb(null, res.text);
+    });
   });
 }
