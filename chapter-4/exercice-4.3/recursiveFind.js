d11345056cb0a10f1bbbc0348fc7e7fbae7a8507
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-12-23 10:55:39 +0300

    add recursiveFind

--- chapter-4/exercice-4.3/recursiveFind.js
+++ chapter-4/exercice-4.3/recursiveFind.js
@@ -1,42 +1,78 @@
+import { count } from "console";
 import fs from "fs";
 import path from "path";
 
+// let result = [];
+// function recursiveFind(dir, keyword, cb) {
+//   fs.readdir(dir, "utf8", (err, filePaths) => {
+//     if (err) {
+//       return cb(err);
+//     }
+
+//     let index = 0;
+//     function iterator(index) {
+//       if (index === filePaths.length) {
+//         return cb(null, result);
+//       }
+
+//       const file = filePaths[index];
+
+//       fs.readFile(path.join(dir, file), "utf8", (err, content) => {
+//         if (err) {
+//           return cb(err);
+//         }
+
+//         if (content.indexOf(keyword) > -1) {
+//           result.push(file);
+//         }
+
+//         index++;
+//         iterator(index);
+//       });
+//     }
+
+//     iterator(0);
+//   });
+// }
+
+// recursiveFind("./test", "batman", (err, data) => {
+//   if (err) {
+//     console.log("ooops");
+//   } else {
+//     console.log("data=", data);
+//   }
+// });
+
 let result = [];
+let totalCount = 0;
+let currentCount = 0;
+let firsEntry = true;
 function recursiveFind(dir, keyword, cb) {
   fs.readdir(dir, "utf8", (err, filePaths) => {
-    if (err) {
-      return cb(err);
-    }
-
-    let index = 0;
-    function iterator(index) {
-      if (index === filePaths.length) {
-        return cb(null, result);
-      }
-
-      const file = filePaths[index];
-
-      fs.readFile(path.join(dir, file), "utf8", (err, content) => {
+    if (err && err.code == "ENOTDIR") {
+      fs.readFile(dir, "utf8", (err, fileContent) => {
         if (err) {
           return cb(err);
         }
 
-        if (content.indexOf(keyword) > -1) {
-          result.push(file);
+        if (fileContent.indexOf(keyword) > -1) {
+          result.push(dir);
         }
-
-        index++;
-        iterator(index);
+        console.log(result);
       });
+    } else {
+      totalCount++;
+      for (const file of filePaths) {
+        currentCount++;
+        recursiveFind(path.join(dir, file), keyword, cb);
+      }
     }
-
-    iterator(0);
   });
 }
 
 recursiveFind("./test", "batman", (err, data) => {
   if (err) {
-    console.log("ooops");
+    console.log("ooops", err);
   } else {
     console.log("data=", data);
   }
