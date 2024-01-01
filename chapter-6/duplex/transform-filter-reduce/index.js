d780ac556bafac8a6720a668077a1a17a8d784ad
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-01-01 16:44:20 +0300

    add passthrough stream

--- chapter-6/duplex/transform-filter-reduce/index.js
+++ chapter-6/duplex/transform-filter-reduce/index.js
@@ -3,11 +3,13 @@ import { createGunzip } from "zlib";
 import { parse } from "csv-parse";
 import { FilterByCountry } from "./filter-by-country.js";
 import { SumProfit } from "./sum-profit.js";
+import monitor from "../../passthrough-monitoring/index.js";
 
 const csvParser = parse({ column: true, delimiter: "," });
 
 createReadStream("./data.csv")
   .pipe(csvParser)
+  // .pipe(monitor)
   .pipe(new FilterByCountry("Italy"))
   .pipe(new SumProfit())
   .pipe(process.stdout);
